8dc3102e7eddbf6ebdf9a794d4e8d01c80464846
SimoneS93 <salernosimone.uni@gmail.com>
2015-06-25 09:44:32 +0200

    Stays inactive on AJAX requests, non-GET requests, if cart is not empty

--- xtremecache.php
+++ xtremecache.php
@@ -1,6 +1,13 @@
 <?php
 
+/**
+ * Serve cached pages with no request processing
+ * @author Salerno Simone
+ * @version 1.0.1
+ * @license MIT
+ */
 class XtremeCache extends Module {
+    const CACHE_TTL = 18000; //Cache Time-To-Live
     
     public function __construct() {
         $this->name = 'xtremecache';
@@ -19,6 +26,16 @@ class XtremeCache extends Module {
         return parent::install() && 
                 $this->registerHook('actionDispatcher') &&
                 $this->registerHook('actionRequestComplete');
+    }
+    
+    public function uninstall() {
+        //delete all cached files
+        $dirname = dirname($this->getCacheFilename('fake_url'));
+        array_walk(scandir($dirname), 'unlink');
+        
+        return $this->unregisterHook('actionDispatcher') &&
+                $this->unregisterHook('actionRequestComplete') &&
+                parent::uninstall();
     }
     
     /**
@@ -53,14 +70,15 @@ class XtremeCache extends Module {
         $controller = $params['controller'];
         
         if (is_subclass_of($controller, 'FrontController')) {
-            require_once $this->path(_PS_TOOL_DIR_.'minify_html/minify_html.class.php');
+            require_once(_PS_TOOL_DIR_.'minify_html'.DS.'minify_html.class.php');
             
             $output = Minify_HTML::minify($params['output']);
             $filename = $this->getCacheFilename();
             $dirname = dirname($filename);
             
             //create cache dir if not exists
-            if (!is_dir($dirname)) mkdir($dirname, 0666);
+            if (!is_dir($dirname))
+                mkdir($dirname, 0666);
             
             file_put_contents($filename, $output);
 			
@@ -72,10 +90,18 @@ class XtremeCache extends Module {
      * @return boolean
      */
     private function isActive() {
-		$active = true;
-        $customer = Context::getContext()->customer;
+        $active = !Tools::getValue('ajax', false);
+        $active = $active && filter_input(INPUT_REQUEST, 'REQUEST_METHOD') === 'GET';
+        
+        //check that customer is not logged in
+        $customer = $this->context->customer;
         if ($customer && $customer instanceof Customer)
             $active = $active && !$customer->isLogged();
+        
+        //for guest checkout, check that cart is empty
+        $cart = $this->context->cart;
+        if ($cart && $cart instanceof Cart)
+            $active = $active && empty($cart->getProducts());
 		
         return $active;
     }
@@ -87,8 +113,7 @@ class XtremeCache extends Module {
         $filename = $this->getCacheFilename();
         
         //check age validity. Delete old cache files
-        
-        if (time() - filectime($filename) > 3*3600) {
+        if (file_exists($filename) && time() - filectime($filename) > static::CACHE_TTL) {
             @unlink($filename);
             return FALSE;
         }
@@ -101,20 +126,10 @@ class XtremeCache extends Module {
      * @param string $url
      * @return string
      */
-    private function getCacheFilename() {
-        return $this->path(sprintf(
-                '%s/xcache/%s.html', 
-                __DIR__, 
-                md5(filter_input(INPUT_SERVER, 'REQUEST_URI'))
-        ));
-    }
-    
-    /**
-     * Use correct DS for paths
-     * @param string $path
-     * @return string
-     */
-    private function path($path) {
-        return str_replace('/', DS, $path);
+    private function getCacheFilename($url=null) {
+        if ($url === null)
+            $url = filter_input(INPUT_SERVER, 'REQUEST_URI');
+        
+        return __DIR__.DS.'xcache'.DS.md5($url).'.html';
     }
 }
